3f0cb87e7ed3722946aecf34b0c4b98c38b14861
Vitya Max <kislovheart@gmail.com>
2019-10-22 20:31:56 +0300

    Rerout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     if @user.save
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+      redirect_to login_path
     # Failed signup
     else
       render "new"
